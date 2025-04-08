c5264511699e723e181fc5cd64e4a1af6f1b756a
Muhammad Umair <muhammadumair25591@gamil.com>
2025-04-08 23:15:14 +0500

    init

---
+++ assets/js/load-component.js
@@ -0,0 +1,25 @@
+function adjustMainContentPadding() {
+    const navbar = document.querySelector('.main-nav-box');
+    const mainContent = document.querySelector('.main-content');
+
+    if (navbar && mainContent) {
+        const navbarHeight = navbar.offsetHeight;
+        console.log(navbarHeight)
+        mainContent.style.paddingTop = `${navbarHeight}px`;
+    }
+}
+
+$(document).ready(function () {
+    $("#nav-placeholder").load("./assets/components/header.html");
+    setTimeout(() => {
+        adjustMainContentPadding();
+        document.querySelectorAll('.setImagePath').forEach(img => {
+            const filename = img.getAttribute('data-filename');
+            img.src = `${window.location.origin}/assets/images/${filename}`;
+        });
+    }, 500);
+});
+
+$(document).ready(function () {
+    $("#footer-placeholder").load("./assets/components/footer.html");
+});
